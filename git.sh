56d6d506f3738e414bf0399290bba875410ff818
uMtMu <umut1010@gmail.com>
2014-10-08 18:56:59 +0300

    git.sh hata giderildi

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 
 echo "Git de kullanmak istediğiniz eposta adresinizi giriniz"
 read eposta
-git config --global user.email $email
+git config --global user.email $eposta
 echo "Git de kullanmak istediğiniz kullanıcı adınızı giriniz"
 read isim
 git config --global user.name $isim
