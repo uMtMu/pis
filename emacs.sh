13a97c24af288279d501f49f83aaa1221c940962
uMtMu <umut1010@gmail.com>
2016-01-02 16:28:29 +0200

    Missing file in last commit

---
+++ emacs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Either of these
+sudo pip install jedi
+# flake8 for code checks
+sudo pip install flake8
+# importmagic for automatic imports
+sudo pip install importmagic
+# and autopep8 for automatic PEP8 formatting
+sudo pip install autopep8
+# and yapf for code formatting
+sudo pip install yapf
