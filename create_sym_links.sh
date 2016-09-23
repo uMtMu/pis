cb12d96a71dc267cf9719d269b3ac22a1ed6608e
uMtMu <umut1010@gmail.com>
2016-09-23 14:13:01 +0300

    update for i3 config

--- create_sym_links.sh
+++ create_sym_links.sh
@@ -2,5 +2,11 @@
 dotfiles=$(ls $(pwd)/dotfiles)
 for file in $dotfiles
 do
-    ln -s "$(pwd)/dotfiles/$file" "$HOME/.$file"
+    if $file=="i3config
+    then
+        mkdir "$HOME/i3"
+        ln -s "$(pwd)/dotfiles/$file" "$HOME/.i3/config"
+    else
+        ln -s "$(pwd)/dotfiles/$file" "$HOME/.$file"
+    fi
 done
